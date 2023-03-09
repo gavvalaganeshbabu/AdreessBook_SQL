229b3dd51bcad7632bf1ce6d46cc9952b18d7461
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-03-09 18:43:35 +0530

    Master Code Added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADBook_System_SQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to Address Book System in SQL*****!");
+        }
+    }
+}
