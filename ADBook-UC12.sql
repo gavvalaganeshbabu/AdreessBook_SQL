1e949d4c6a93277966b54f4db40bb560c16124cb
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-03-09 21:58:08 +0530

    Dwawn ER diagram for AdressBook DB

---
+++ ADBook-UC12.sql
@@ -0,0 +1,99 @@
+﻿-------- Creating Tables Based on ER Diagrams  --------
+
+create table Address_Book(
+Address_BookID int identity(1,1) primary key,
+Address_BookName varchar(200)
+)
+insert into Address_Book values ('MARS Book'),('RAMS Book');
+
+select * from Address_Book;
+
+create table Contact_Person(
+AddressBook_ID int,
+Contact_ID int identity(1,1) primary key,
+FirstName varchar(100),
+LastName varchar(100),
+Address varchar(250),
+City varchar(100),
+State varchar(100),
+zip BigInt,
+PhoneNumber BigInt,
+Email varchar(200),
+foreign key (AddressBook_ID) references Address_Book(Address_BookID))
+
+insert into Contact_Person values
+(1,'Vijaya','Macha','645 Catherine Street','Bangalore','Karnataka',243001,9842905050,'vijaya@gmail.com'),
+(2,'Ganesh','Dasari','836 Heritage Rd','Chennai','Tamil Nadu',134002,9840208500,'ganesh@gmail.com'),
+(2,'Keerthi','Ganesh','19 Augusta Avenue','Lucknow','Uttar Pradesh',113201,8721050553,'keerthi@gmail.com'),
+(1,'Srishanth','Dasari','356 Newburgh','Bangalore','Karnataka',243001,9842905050,'srishanth@gmail.com');
+
+select * from Contact_Person;
+
+create table ContactType
+(ContactType_ID int identity(1,1) primary key,
+ContactType_Name varchar(200)
+)
+
+insert into ContactType values
+('Family'),('Friends'),('Profession');
+
+select * from ContactType;
+
+create Table TypeManager(
+ContactType_Identity int,
+Contact_Identity int,
+foreign key (ContactType_Identity) references ContactType(ContactType_ID),
+foreign key (Contact_Identity) references Contact_Person(Contact_ID)
+)
+
+insert into TypeManager values
+(1,3),
+(2,3),
+(3,1),
+(1,2),
+(2,4);
+
+select * from TypeManager;
+
+------ UseCase 11: Create Contact for both Family and Friends Type ------ 
+
+select Address_BookName,Concat(FirstName,' ',LastName) as Name,Concat(Address,' ,',City,' ,',State,' ,',zip) as Address,PhoneNumber,Email,ContactType_Name
+from Address_Book 
+Full JOIN Contact_Person on Address_Book.Address_BookID=AddressBook_ID 
+Full JOIN TypeManager on TypeManager.Contact_Identity=Contact_ID
+Full JOIN ContactType on TypeManager.ContactType_Identity=ContactType_ID
+
+------  UseCase 12------  
+
+-- 1: Ability to Retrieve Person belonging to a City or State --
+
+select Address_BookName,Concat(FirstName,' ',LastName) as Name,Concat(Address,' ,',City,' ,',State,' ,',zip) as Address,PhoneNumber,Email,ContactType_Name
+from Contact_Person 
+INNER JOIN  Address_Book on Address_Book.Address_BookID=AddressBook_ID and (City='Bangalore' or State='Karnataka')
+INNER JOIN TypeManager on TypeManager.Contact_Identity=Contact_ID
+INNER JOIN ContactType on TypeManager.ContactType_Identity=ContactType_ID
+
+------ 2: Ability to Retrieve Count of Person belonging to a City or State ------
+
+select Count(*),state,City
+from Contact_Person 
+INNER JOIN  Address_Book on Address_Book.Address_BookID=AddressBook_ID 
+Group by state,City;
+
+------ 3: Ability to retrieve entries sorted alphabetically ------
+
+select Address_BookName,(FirstName+' '+LastName) as Name,Concat(Address,' ,',City,' ,',State,' ,',zip) as Address,PhoneNumber,Email,ContactType_Name
+from Contact_Person 
+INNER JOIN  Address_Book on Address_Book.Address_BookID=AddressBook_ID and (City='Bangalore')
+INNER JOIN TypeManager on TypeManager.Contact_Identity=Contact_ID
+INNER JOIN ContactType on TypeManager.ContactType_Identity=ContactType_ID
+order by(FirstName);
+
+------ 4: Ability to get number of contact persons by Type------
+
+select Count(*) as NumberOfContacts,ContactType.ContactType_Name
+from Contact_Person 
+INNER JOIN  Address_Book on Address_Book.Address_BookID=AddressBook_ID
+INNER JOIN TypeManager on TypeManager.Contact_Identity=Contact_ID
+INNER JOIN ContactType on TypeManager.ContactType_Identity=ContactType_ID
+Group by ContactType_Name;
